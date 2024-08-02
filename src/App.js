d00b5dbb2b0d96edc66a15248772788b40e61993
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-08-02 09:16:12 +0200

    Added Bootstrap & footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nina Delvaux using React. It is open-sourced
+          on Github and hosted on Netlify
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
