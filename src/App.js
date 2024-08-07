d193fb7ba81407ca802ce9b57d999a45d400df7e
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-08-07 18:34:17 +0200

    Added links in footer

--- src/App.js
+++ src/App.js
@@ -8,8 +8,30 @@ export default function App() {
       <div className="container-fluid">
         <Weather defaultCity="Brussels" />
         <footer>
-          This project was coded by Nina Delvaux using React. It is open-sourced
-          on Github and hosted on Netlify
+          This project was coded by{" "}
+          <a
+            href="https://github.com/NinaDlvx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nina Delvaux
+          </a>{" "}
+          using React. It is{" "}
+          <a
+            href="https://github.com/NinaDlvx/my-reactweatherapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on Github and hosted on{" "}
+          <a
+            href="https://myreactweatherapplication.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
