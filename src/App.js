cbbbdb4a294972973f6d1293d4e25ac36160b6fb
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-08-08 19:23:28 +0200

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -8,30 +8,32 @@ export default function App() {
       <div className="container-fluid">
         <Weather defaultCity="Brussels" />
         <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/NinaDlvx"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Nina Delvaux
-          </a>{" "}
-          using React. It is{" "}
-          <a
-            href="https://github.com/NinaDlvx/my-reactweatherapp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced
-          </a>{" "}
-          on Github and hosted on{" "}
-          <a
-            href="https://myreactweatherapplication.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/NinaDlvx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Nina Delvaux
+            </a>{" "}
+            using React. <br /> It is{" "}
+            <a
+              href="https://github.com/NinaDlvx/my-reactweatherapp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced
+            </a>{" "}
+            on Github and hosted on{" "}
+            <a
+              href="https://myreactweatherapplication.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </p>
         </footer>
       </div>
     </div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 import "./Weather.css";
@@ -16,29 +16,35 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function loadApi() {
+    let apiKey = `a20035dd93a6tbea3b9cff448a2b7eeo`;
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
           <h3>Extended Forecast</h3>
           {forecast.map(function (dailyForecast, index) {
-            if (index < 5)
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
+            }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = `a20035dd93a6tbea3b9cff448a2b7eeo`;
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    loadApi();
     return "Loading forecast...";
   }
 }
